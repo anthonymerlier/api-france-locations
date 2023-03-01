cdababeabeb79a85e641b02be287c9438ffa0480
Alexandre Vurbier <alexandre.vurbier@gmail.com>
2023-03-01 01:55:52 +0100

    Added convertStringToRegexp function

--- utils/validation.utils.js
+++ utils/validation.utils.js
@@ -1,7 +1,44 @@
-export const isLatitude = (lat) => {
+/**
+ * Check if `lat` is a valid decimal degree latitude
+ * @param {*} lat Latitude
+ */
+
+export const isDecimalDegreeLatitude = (lat) => {
     return isFinite(lat) && Math.abs(lat) <= 90;
 }
 
-export const isLongitude = (lon) => {
+/**
+ * Check if `lon` is a valid decimal degree longitude
+ * @param {*} lon Longitude
+ */
+
+export const isDecimalDegreeLongitude = (lon) => {
     return isFinite(lon) && Math.abs(lon) <= 180;
 }
+
+/**
+ * Replace all possibly accentuated characters, white spaces and hyphens to match partial MongoDB search.
+ ** Example: `Saône-et-Loire` => `s[a,á,à,ä,â,ã][o,ó,ö,ò,õ,ô][n,ñ][e,é,ë,è,ê][ -][e,é,ë,è,ê]t[ -]l[o,ó,ö,ò,õ,ô][i,í,ï,ì,î]r[e,é,ë,è,ê]`
+ * @param {*} text Text to convert
+ */
+
+export const convertStringToRegexp = (text) => {
+    let regexp = '';
+    const textNormalized = text
+        .normalize('NFD') // returns the Unicode Normalization Form of the string
+        .replace(/[\u0300-\u036f]/g, '') // remove all accents
+        .replace(/[|\\{}()[\]^$+*?.]/g, '\\$&') // remove all regexp reserved char
+        .toLowerCase(); // transform text to lowercase
+
+    regexp = textNormalized
+        .replace(/a/g, '[a,á,à,ä,â,ã]')
+        .replace(/e/g, '[e,é,ë,è,ê]')
+        .replace(/i/g, '[i,í,ï,ì,î]')
+        .replace(/o/g, '[o,ó,ö,ò,õ,ô]')
+        .replace(/u/g, '[u,ü,ú,ù,û]')
+        .replace(/c/g, '[c,ç]')
+        .replace(/n/g, '[n,ñ]')
+        .replace(/[ªº°]/g, '[ªº°]')
+        .replace(/-|\s/g, '[ -]') // replace spaces and hyphens
+    return new RegExp(regexp, 'i'); // "i" -> ignore case
+};
