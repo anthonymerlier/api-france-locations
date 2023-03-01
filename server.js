d22e33b12c460aa8fc9368c5a6c23730c1dd0b0d
Alexandre Vurbier <alexandre.vurbier@gmail.com>
2023-03-01 14:24:45 +0100

    Added helmet, rate-limit and express-sanitizer

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
+import helmet from 'helmet'
+import { rateLimit } from 'express-rate-limit'
+import ExpressMongoSanitize from 'express-mongo-sanitize'
 import dotenv from 'dotenv'
 dotenv.config({ path: './config/config.env' })
 import './config/db.js'
+
 import locationsRoutes from './api/locations/location.api.routes.js'
 import departmentsRoutes from './api/departments/departments.api.routes.js'
 import regionsRoutes from './api/regions/regions.api.routes.js'
@@ -12,26 +16,45 @@ import newRegionsRoutes from './api/new-regions/new_regions.api.routes.js'
 const app = express();
 
 app.use(cors({
-    mode: 'no-cors',
-    credentials: true,
-    origin: '*',
-    "Access-Control-Allow-Origin": '*',
-    'allowedHeaders': ['sessionId', 'Content-Type', 'Authorization', 'application'],
-    'exposedHeaders': ['sessionId'],
-    'methods': 'GET, OPTIONS, HEAD, PUT, PATCH, POST, DELETE',
+    'credentials': true,
+    'origin': '*',
+    "Access-Control-Allow-Origin": true,
+    'allowedHeaders': ['Content-Type', 'Authorization', 'application'],
+    'methods': 'GET',
     'preflightContinue': false,
 }))
-app.use(express.json({ limit: '50mb' }))
+
+app.use(helmet())
+
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 150,
+    standardHeaders: true,
+    legacyHeaders: false,
+    message: 'Too Many Request from this IP, please try again in an 15 minutes'
+})
+
+app.use(limiter)
+
+app.use(express.json({ limit: '15kb' }))
 app.use(bodyParser.urlencoded({
     extended: false,
-    limit: '50mb'
+    limit: '15kb'
 }))
-app.use(bodyParser.json({ limit: '50mb' }))
+app.use(bodyParser.json({ limit: '15kb' }))
+
+app.use(ExpressMongoSanitize({
+    allowDots: true,
+}));
 
 app.use('/search/locations', locationsRoutes)
 app.use('/search/departments', departmentsRoutes)
 app.use('/search/regions', regionsRoutes)
 app.use('/search/new-regions', newRegionsRoutes)
+
+app.get("*", async (req, res) => {
+    return res.status(400).json({ error: `Cette route n'existe pas.` })
+})
 
 if (process.env.NODE_ENV !== 'production') {
     process.once('uncaughtException', err => {
@@ -39,6 +62,14 @@ if (process.env.NODE_ENV !== 'production') {
         setTimeout(() => process.exit(1), 100)
     })
 }
+
+process.on('unhandledRejection', err => {
+    console.log('Erreur :');
+    console.log(err.name, err.message);
+    server.close(() => {
+        process.exit(1);
+    });
+});
 
 const PORT = process.env.PORT || 5000
 
